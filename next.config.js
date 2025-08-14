e2bdb170ef910039e83b2134ac61b5280b128114
arjun7976 <arjunprajapati7976@gmail.com>
2025-08-14 17:57:24 +0530

    add project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
   },
   typescript: {
     ignoreBuildErrors: true,
@@ -9,6 +12,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Optional: Add a trailing slash to all paths
+  trailingSlash: true,
+  // Optional: Enable React Strict Mode
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
